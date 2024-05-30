d43b55225c03ca1105dc6be432be12d0a6935fa5
alfanaisy <alfanaisy7@gmail.com>
2024-05-30 14:23:52 +0700

    Add loading skeleton for detail and new page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { Box, Card, Flex, Heading, Skeleton } from '@radix-ui/themes';
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Heading>
+        <Skeleton>This is for issue title</Skeleton>
+      </Heading>
+      <Box className="flex space-x-5">
+        <Skeleton>Text</Skeleton>
+        <Skeleton>Created Date</Skeleton>
+      </Box>
+      <Card className="prose" mt={'4'}>
+        <Skeleton>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ut nulla
+          mollitia voluptates accusantium veritatis explicabo sapiente rem odio
+          ipsum illo beatae cumque quos dolore, excepturi voluptatum quis facere
+          vitae eos totam adipisci. Reprehenderit impedit, sequi distinctio
+          veniam mollitia reiciendis totam cupiditate aspernatur quos animi.
+          Possimus, enim eius maiores sit odio quos porro molestias in nam
+          sequi. Voluptatibus veniam fuga provident veritatis odio voluptatem,
+          illum ullam facilis deserunt aspernatur minima dignissimos consequatur
+          magni cumque corrupti mollitia, fugit esse obcaecati voluptatum
+          consequuntur hic, eligendi numquam nulla odit? Expedita, voluptatibus
+          inventore eaque voluptatem eius placeat quod rem, quaerat deleniti
+          necessitatibus, recusandae debitis.
+        </Skeleton>
+      </Card>
+    </div>
+  );
 };
 
 export default LoadingIssueDetailPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,16 @@
+import { Box, Button, Skeleton, Spinner } from '@radix-ui/themes';
 import React from 'react';
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl space-y-3">
+      <Skeleton width={'100%'} height={'30px'}></Skeleton>
+      <Skeleton width={'100%'} height={'340px'}></Skeleton>
+      <Button className="w-max" disabled>
+        <Spinner />
+      </Button>
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;
