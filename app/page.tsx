cd6f58b0ddf6c2ab6aded61afdfd5655d2ab2b52
alfanaisy <alfanaisy7@gmail.com>
2024-06-03 14:08:56 +0700

    Built layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Pagination } from './components';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+    </>
+  );
 }
